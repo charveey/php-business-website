b2d02063ccdbd98f2ad060a251bba4dfa225def6
charveey <amlamanangekevin@gmail.com>
2025-09-15 11:09:00 +0000

    Added htaccess and fixed upload size

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -89,7 +89,7 @@ define( 'WP_DEBUG', false );
 
 /* Add any custom values between this line and the "stop editing" line. */
 
-
+@ini_set('upload_max_size' , '256M' );
 
 /* That's all, stop editing! Happy publishing. */
 
